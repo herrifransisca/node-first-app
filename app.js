2dbf835851c4bf40496b7d67608aa288f1baccbe
herrifransisca <herrifransisca@gmail.com>
2018-11-05 17:46:52 +0700

    02-05-Loading a Module

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log(module);
+/* #1 */
+// var logger = require('./logger');
+
+// logger.log('message');
+
+/* #2 */
+// var logger = require('./logger');
+
+// logger = 1;
+
+// logger.log('message');  // TypeError: logger.log is not a function
+
+/* #3 */
+// const logger = require('./logger');
+
+// logger = 1;
+
+// logger.log('message');  // TypeError: assignment to constant variable
+
+// tool: jshint app.js
+
+/* #4 */
+
+// const logger = require('./logger');
+
+// logger('message');
+
+/* #5 */
+
+const log = require('./logger');
+
+log('message');
