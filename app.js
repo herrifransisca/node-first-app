0ca4c039e2e1562589c18f91e336be7b6e9cf5bf
herrifransisca <herrifransisca@gmail.com>
2018-11-05 21:07:36 +0700

    02-07-Path Module

--- app.js
+++ app.js
@@ -1,32 +1,5 @@
-/* #1 */
-// var logger = require('./logger');
+const path = require('path');
 
-// logger.log('message');
+const pathObj = path.parse(__filename);
 
-/* #2 */
-// var logger = require('./logger');
-
-// logger = 1;
-
-// logger.log('message');  // TypeError: logger.log is not a function
-
-/* #3 */
-// const logger = require('./logger');
-
-// logger = 1;
-
-// logger.log('message');  // TypeError: assignment to constant variable
-
-// tool: jshint app.js
-
-/* #4 */
-
-// const logger = require('./logger');
-
-// logger('message');
-
-/* #5 */
-
-const log = require('./logger');
-
-log('message');
+console.log(pathObj);
