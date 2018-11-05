6c1320e8d41c66272ae633e0797100e2a067db5f
herrifransisca <herrifransisca@gmail.com>
2018-11-05 19:48:23 +0700

    02-06-Module Wrapper Function

--- logger.js
+++ logger.js
@@ -1,3 +1,58 @@
+/* #1 */
+// var url = 'http://myLogger.io/log';
+
+// function log(message) {
+//   // Send an HTTP request
+//   console.log(message);
+// }
+
+// module.exports = log;
+
+/* #2 */
+// var x =;  // unexpected token
+// var url = 'http://myLogger.io/log';
+
+// function log(message) {
+//   // Send an HTTP request
+//   console.log(message);
+// }
+
+// module.exports = log;
+
+/* #3 */
+// (function(exports, require, module, __filename, __dirname) {
+//   var url = 'http://myLogger.io/log';
+
+//   function log(message) {
+//     // Send an HTTP request
+//     console.log(message);
+//   }
+
+//   module.exports = log;
+// });
+
+/* #4 */
+// (function(exports, require, module, __filename, __dirname) {
+//   var url = 'http://myLogger.io/log';
+
+//   function log(message) {
+//     // Send an HTTP request
+//     console.log(message);
+//   }
+
+//   module.exports = log;
+
+//   module.exports.log = log;
+//   exports.log = log;
+
+//   // CANNOT
+//   exports = log;  // module.exports
+// });
+
+/* #5 */
+console.log(__filename);
+console.log(__dirname);
+
 var url = 'http://myLogger.io/log';
 
 function log(message) {
@@ -5,5 +60,4 @@ function log(message) {
   console.log(message);
 }
 
-// module.exports.log = log;
 module.exports = log;
