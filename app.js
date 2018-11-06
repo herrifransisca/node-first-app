1bac5c92982acfc4ce77dc55f7d46c39554cda4a
herrifransisca <herrifransisca@gmail.com>
2018-11-06 20:25:25 +0700

    02-13-HTTP Module

--- app.js
+++ app.js
@@ -1,9 +1,55 @@
-const Logger = require('./logger');
-const logger = new Logger();
+/* #1 */
+// const http = require('http');
 
-// Register a listener
-logger.on('messageLogged', arg => {
-  console.log('logged', arg);
+// const server = http.createServer();
+
+// server.listen(3000);
+
+// console.log('Listening on port 3000...');
+
+/* #2 */
+// const http = require('http');
+
+// const server = http.createServer();
+
+// server.on('connection', socket => {
+//   console.log('New connection...');
+// });
+
+// server.listen(3000);
+
+// console.log('Listening on port 3000...');
+
+/* #3 */
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === '/') {
+//     res.write('Hello World');
+//     res.end();
+//   }
+// });
+
+// server.listen(3000);
+
+// console.log('Listening on port 3000...');
+
+/* #4 */
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello World');
+    res.end();
+  }
+
+  if (req.url === '/api/courses') {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
 });
 
-logger.log('message');
+server.listen(3000);
+
+console.log('Listening on port 3000...');
